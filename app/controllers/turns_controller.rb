5122fef4177bf469f5638dee705b51bfd198e094
Agustin Salum <agustinsalum92@hotmail.com>
2022-11-22 18:12:30 -0300

    Se listan los turnos del usuario autenticado

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -1,6 +1,9 @@
 class TurnsController < ApplicationController
+
   def index
-    @turnos_usuario_actual = Turn.all
+    # El usuario actual lo obtenemos con current_user
+    todos_turnos = Turn.all
+    @turnos_usuario_actual = todos_turnos.select { |un_turno| un_turno.user_client_id == current_user.id}
   end
 
   def show
