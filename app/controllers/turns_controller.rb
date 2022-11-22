6ece7d89718b5f9e9e9b7052a12c6da0b1fe3c4f
Agustin Salum <agustinsalum92@hotmail.com>
2022-11-22 00:26:00 -0300

    Agregado comentarios

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -4,6 +4,7 @@ class TurnsController < ApplicationController
   end
 
   def show
-    @mis_turnos = Turn.find(params[:id])
+    @mis_turnos = Turn.all
+    #@mis_turnos = Turn.find(params[:id])
   end
 end
